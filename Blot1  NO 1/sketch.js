6f323280270ec9acd3b29718cc952e84afc56e4a
p5jsBlot <88648362+p5jsBlot@users.noreply.github.com>
2021-10-02 09:43:00 +0300

    Upload p5.js sketch files from my compurer
    I am making a video...

---
+++ Blot1  NO 1/sketch.js
@@ -0,0 +1,11 @@
+function setup(){
+    var canvas= createCanvas (400,400);
+  
+   }
+   
+   function draw(){
+   
+     background(0);
+     fill (250,150,300,random)
+     ellipse (200,200,random(3500),random(400))
+  }
